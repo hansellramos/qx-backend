563a09297d4c25825c2877bb5f791f1580cf6aed
Hansel Ramos <hansel.ramos@yuxipacific.com>
2016-05-21 09:22:32 -0500

    - Modified Auth post response

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,14 +3,24 @@ var auth_model = require('../models/auth');
 var sha1 = require('sha1');
 var router = express.Router();
 
-router.post('/', function(req, res, next){
+router.post('/:token', function(req, res, next){
     auth_model.authenticate(req.body.username, sha1(req.body.password), function(error, doc){
         if(error){ cb(error);
         }else{
             if(doc){
-                console.log("auth");
+                res.json({
+                    success:true,
+                    message:'',
+                    data:{
+                        token:'==='
+                    }
+                });
             }else{
-                console.log("invalid username and password");
+                res.status(401).json({
+                    success:false,
+                    message:'Con los datos proporcionados no ha sido posible identificarte, por favor verifica e intenta de nuevo',
+                    data:{}
+                })
             }
         }
     })
