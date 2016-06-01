12bc586257f3421f52e1d23351a5b3ed166bf2be
Hansel Ramos <hansel.ramos@yuxipacific.com>
2016-06-01 18:01:11 -0500

    Added aggregate function to record model

--- models/record.js
+++ models/record.js
@@ -3,10 +3,60 @@ var db = require('../db');
 exports.all = function (product, cb) {
     var items = [];
     db.get()
-        .collection('record').find(
-        {product:product.id}
-        , {id:0,properties:0}
-        )
+        .collection('record').aggregate([
+            {
+                $lookup: {
+                    from: 'user'
+                    , localField: 'user'
+                    , foreignField: 'id'
+                    , as: 'user'
+                }
+            }
+            , {
+                $lookup: {
+                    from: 'external'
+                    , localField: 'supplier'
+                    , foreignField: 'id'
+                    , as: 'supplier'
+                }
+            }
+            , {
+                $match: {
+                    product: product.id
+                }
+            }
+            , {
+                $project: {
+                    _id: 1,
+                    reference: 1,
+                    product: 1,
+                    analysis_date: 1,
+                    elaboration_date: 1,
+                    due_date: 1,
+                    reception_date: 1,
+                    properties: {
+                        property: 1,
+                        value: 1
+                    },
+                    user: {
+                        _id: 1,
+                        id: 1,
+                        firstname: 1,
+                        lastname: 1
+                    },
+                    veredict: 1,
+                    remission: 1,
+                    quantity: 1,
+                    existing_quantity: 1,
+                    supplier: {
+                        name: 1
+                    },
+                    satisfies: 1,
+                    active: 1,
+                    clause: 1
+                }
+            }
+        ])
         .toArray(function (err, docs) {
             cb(err, docs);
         });
