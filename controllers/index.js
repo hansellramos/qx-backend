48877dd3a63018e47611119230bf3e2f6162817d
Hansel Ramos <hansell.ramos@globant.com>
2017-03-04 19:36:20 -0500

    Modified index

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var package = require('../package.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+      title: 'Qualitrix API'
+      , description:package.description
+      , version:package.version
+      , lastUpdateDate:package.lastUpdateDate
+      , release:package.release
+      , author:package.author
+      , year: 2017
+  });
 });
 
 module.exports = router;
